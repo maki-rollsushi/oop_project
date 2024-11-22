483b86fd493f5480225e50fa8557ebe85510cd57
makiroll.mark <delacruzmacky39@gmail.com>
2024-11-22 09:25:53 +0800

    just created the function

--- InventoryManagement.java
+++ InventoryManagement.java
@@ -1,5 +1,19 @@
 package inventory;
 
 public class InventoryManagement{
-  
+    //attribute declarations
+
+    private
+
+    public InventoryManagement{
+        System.out.println("INVENTORY MANAGEMENT");     
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+    }
+
 }
